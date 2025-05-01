67af1acc4854fbd224e4e9f549891452c92e763e
QMODEM <kakros918@protonmail.com>
2025-05-01 22:33:06 +0800

    Update 02_path.sh

--- config/immortalwrt-openwrt-24.10/02_path.sh
+++ config/immortalwrt-openwrt-24.10/02_path.sh
@@ -7,7 +7,6 @@
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
 echo "DISTRIB_SOURCECODE='ImmortalWRT'" >>package/base-files/files/etc/openwrt_release
 
-#!/bin/bash
 
 mkdir -p feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include
 curl -o feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js https://raw.githubusercontent.com/intannajwa/imo-path/main/24.10.0/10_system.js
