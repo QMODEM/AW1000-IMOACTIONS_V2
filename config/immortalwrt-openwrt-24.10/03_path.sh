168dd8ac10b86f99cac7ae0460fa2ab86c186b76
QMODEM <kakros918@protonmail.com>
2025-05-01 20:00:24 +0800

    Update 03_path.sh

--- config/immortalwrt-openwrt-24.10/03_path.sh
+++ config/immortalwrt-openwrt-24.10/03_path.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Clean old packages from both luci and modem feeds
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/modem/applications/luci-app-passwall
+# rm -rf feeds/luci/applications/luci-app-passwall
+# rm -rf feeds/modem/applications/luci-app-passwall
 
-rm -rf feeds/luci/applications/luci-app-openclash
-rm -rf feeds/modem/applications/luci-app-openclash
+# rm -rf feeds/luci/applications/luci-app-openclash
+# rm -rf feeds/modem/applications/luci-app-openclash
  
-git clone https://github.com/intannajwa/mamamiya.git
+# git clone https://github.com/intannajwa/mamamiya.git
 
-cp -r mamamiya/luci-app-passwall feeds/luci/applications/
-cp -r mamamiya/luci-app-openclash feeds/luci/applications/ 
+# cp -r mamamiya/luci-app-passwall feeds/luci/applications/
+# cp -r mamamiya/luci-app-openclash feeds/luci/applications/ 
 
-rm -rf mamamiya
+# rm -rf mamamiya
 
 # ./scripts/feeds update luci
 # ./scripts/feeds install -f -p luci luci-app-passwall luci-app-openclash luci-app-qbittorrent
